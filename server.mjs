be125212d2255d58045a6230744720e75cbd68a9
Tobias Schmücker <schmuecker.tobias@gmail.com>
2022-11-03 08:24:09 +0100

    Add routes

--- server.mjs
+++ server.mjs
@@ -1,6 +1,16 @@
-import { createServer } from 'http';
+import { createServer } from "http";
 
 createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
+  res.writeHead(200, { "Content-Type": "text/html" }); // http header
+  const url = req.url;
+  if (url === "/about") {
+    res.write("<h1>about us page<h1>"); //write a response
+    res.end(); //end the response
+  } else if (url === "/contact") {
+    res.write("<h1>contact us page<h1>"); //write a response
+    res.end(); //end the response
+  } else {
+    res.write("<h1>Hello World!<h1>"); //write a response
+    res.end(); //end the response
+  }
 }).listen(process.env.PORT);
