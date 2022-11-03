4dddd79395997093399d9da705826c2e5952d137
Tobias Schmücker <schmuecker.tobias@gmail.com>
2022-11-03 08:31:11 +0100

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/ping", async (req, res) => {
+app.get("/", async (req, res) => {
   res.json({ hello: "world" });
 });
 
